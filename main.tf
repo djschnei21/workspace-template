ca2ed740be1a8281d29aacb3f8f2ececc3a2341e
djschnei21 <32646250+djschnei21@users.noreply.github.com>
2022-07-13 15:45:40 -0400

    automate doormat provider config

--- main.tf
+++ main.tf
@@ -1,1 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+    }
+    doormat = {
+      source  = "doormat.hashicorp.services/hashicorp-security/doormat"
+      version = "~> 0.0.2"
+    }
+  }
+}
 
+provider "doormat" {}
+
+provider "aws" {
+  access_key = data.doormat_aws_credentials.creds.access_key
+  secret_key = data.doormat_aws_credentials.creds.secret_key
+  token      = data.doormat_aws_credentials.creds.token
+}
