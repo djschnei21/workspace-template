0fbf7dc7dbf245b65e647ae581923e6e26873135
djschnei21 <32646250+djschnei21@users.noreply.github.com>
2022-12-07 08:08:30 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,16 +3,6 @@ terraform {
     aws = {
       source  = "hashicorp/aws"
     }
-    doormat = {
-      source  = "doormat.hashicorp.services/hashicorp-security/doormat"
-      version = "~> 0.0.2"
-    }
   }
 }
 
-provider "aws" {
-  access_key = data.doormat_aws_credentials.creds.access_key
-  secret_key = data.doormat_aws_credentials.creds.secret_key
-  token      = data.doormat_aws_credentials.creds.token
-  region     = ""
-}
